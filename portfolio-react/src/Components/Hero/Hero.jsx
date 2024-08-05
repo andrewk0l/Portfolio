63bf152a8eb24ae17ed543fa88e3e22f3bf83e62
andrewk0l <andyoged92@gmail.com>
2024-08-05 05:21:13 +0100

    adding and styling the hero section

---
+++ portfolio-react/src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.jpg'
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt='' className='profile-image'/>
+        <h1>Oluwanifemi Ogedengbe, Computer Engineering Graduate</h1>
+        <p>i am a 22 year old, recently graduated with a degree in computer enginnering. i'm wonderfully inquisitive and i am looking to improve myself in anyway possible</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with me</div>
+            <div className="hero-resume">My Resume</div> 
+        </div>
+    </div>
+  )
+}
+
+export default Hero
