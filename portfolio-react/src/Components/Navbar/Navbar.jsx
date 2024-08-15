b3eef1002386c51dde5215b955530e1ca4af278b
andrewk0l <andyoged92@gmail.com>
2024-08-15 20:10:48 +0100

    adding responsiveness

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 import logo from '../../assets/logo.svg'
 import line from '../../assets/line.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_open from '../../assets/menu_open.png'
+import menu_close from '../../assets/menu_close.png'
 
 const Navbar = () => {
 
@@ -12,7 +14,9 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img className='logo' src={logo} alt='' />
+      <img src={menu_open} alt='' className='nav-mob-open'/>
       <ul className="nav-menu">
+        <img src={menu_close} alt='' className='nav-mob-close'/>
         <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu === "home" && <img src={line} alt='' />}</li>
         <li><AnchorLink className='anchor-link' offset={0} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" && <img src={line} alt='' />}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#achievements'><p onClick={() => setMenu("achievements")}>Achievements</p></AnchorLink>{menu === "achievements" && <img src={line} alt='' />}</li>
